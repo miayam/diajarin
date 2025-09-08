f049a51cf8570f70ad42371e65989cf398be8314
Muhamad D. R. <muhammaddeni90@gmail.com>
2025-09-08 20:16:10 +0700

    feat: initialize Astro + Tina CMS template

---
+++ src/components/react/FormattedDate.tsx
@@ -0,0 +1,14 @@
+function FormattedDate({ date }: { date: string }) {
+  const _date = new Date(date);
+  return (
+    <time dateTime={_date.toISOString()}>
+      {_date.toLocaleDateString("en-us", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })}
+    </time>
+  );
+}
+
+export default FormattedDate;
